249d7f3fdfb2817fa84eaad247e404126059c5c0
NundaElroy <sekyezaelroy@gmail.com>
2024-07-16 14:01:25 +0300

    changed the profile pic of the admin

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -96,7 +96,7 @@
                             <div class="card-body">
                                 <div class="author">
                                     <a href="#">
-                                        <img class="avatar border-gray" src="{{ asset('light-bootstrap/img/faces/face-3.jpg') }}" alt="...">
+                                        <img class="avatar border-gray" src="{{ asset('image/profile-img.jpg')}}" alt="...">
                                         <h5 class="title">{{ __('SEBABE@619') }}</h5>
                                     </a>
                                     <p class="description">
