7673d67e65d9448a7c90d74242046c90b482ee80
Ainebyona Alvin <148852363+Alvin170803@users.noreply.github.com>
2024-07-25 14:37:04 +0300

    added theme style bg_info to display via analytics

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -1,43 +1,29 @@
-<nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute">
-    <body>
-    
-    
+<nav class="navbar navbar-expand-lg navbar-dark bg-info">
     <div class="container">
-    
         <div class="navbar-wrapper">
-            <a class="navbar-brand"  href="#pablo">{{ __('Mathematics competition') }}</a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-bar burger-lines"></span>
-                <span class="navbar-toggler-bar burger-lines"></span>
-                <span class="navbar-toggler-bar burger-lines"></span>
+            <a class="navbar-brand text-white" href="#pablo">{{ __('Mathematics competition') }}</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
         </div>
         <div class="collapse navbar-collapse justify-content-end" id="navbar">
             <ul class="navbar-nav">
-               
                 <li class="nav-item @if($activePage == 'register') active @endif">
-                    <a href="{{ route('register') }}" class="nav-link">
+                    <a href="{{ route('register') }}" class="nav-link text-white">
                         <i class="nc-icon nc-badge"></i> {{ __('Register') }}
                     </a>
                 </li>
                 <li class="nav-item @if($activePage == 'login') active @endif">
-                    <a href="{{ route('login') }}" class="nav-link">
+                    <a href="{{ route('login') }}" class="nav-link text-white">
                         <i class="nc-icon nc-mobile"></i> {{ __('Login') }}
                     </a>
                 </li>
                 <li class="nav-item @if($activePage == 'guest.welcome') active @endif">
-                <a href="{{ route('guest.welcome') }}" class="nav-link">
-                <i class="nc-icon nc-badge"></i>{{ __('Guest View') }}
-            </a>
+                    <a href="{{ route('guest.welcome') }}" class="nav-link text-white">
+                        <i class="fa fa-bar-chart" aria-hidden="true"></i> {{ __('Guest View') }}
+                    </a>
                 </li>
             </ul>
-            </div>
-
-
-            
         </div>
-
-       
-    
-    </body>
+    </div>
 </nav>
